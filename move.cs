4d233530251758ec9785eb13400aefe45a037717
Ashiq Firoz <64600806+ashiq-firoz@users.noreply.github.com>
2021-09-09 19:58:27 +0530

    Initial

--- move.cs
+++ move.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class move : MonoBehaviour
 { 
-    public Transform cam;
+    // code to move a object in unity 3d using the arrow keys on the keyboard
+    
+    public Transform cam; //To link the transform(position data) with the script
     void Update()
     {
-        cam.position = this.transform.position;
-        if(Input.GetKey(KeyCode.UpArrow)){
+        cam.position = this.transform.position; //sets the position of cam to be equal to the position of the object(ball) to which the script is linked
+        
+        if(Input.GetKey(KeyCode.UpArrow)){ // checks if uparrow is pressed
             this.transform.Translate(Vector3.forward*10*Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.DownArrow)){
+        if(Input.GetKey(KeyCode.DownArrow)){ // checks if downarrow is pressed
             this.transform.Translate(Vector3.back*10*Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.LeftArrow)){
+        if(Input.GetKey(KeyCode.LeftArrow)){ // checks if leftarrow is pressed
             this.transform.Translate(Vector3.left*10*Time.deltaTime);
         }
-        if(Input.GetKey(KeyCode.RightArrow)){
+        if(Input.GetKey(KeyCode.RightArrow)){ // checks if rightarrow is pressed
             this.transform.Translate(Vector3.right*10*Time.deltaTime);            
         }
     }
