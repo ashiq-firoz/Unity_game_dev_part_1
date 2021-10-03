3dbc0d7a6a682d923b4b81c0342591fc4c78c99e
Ashiq Firoz <64600806+ashiq-firoz@users.noreply.github.com>
2021-10-03 22:35:58 +0530

    Part1 and part 2

--- move.cs
+++ move.cs
@@ -7,12 +7,13 @@ public class move : MonoBehaviour
     // code to move a object in unity 3d using the arrow keys on the keyboard
     
     public Transform cam; //To link the transform(position data) with the script
+    pubic float speed = 10 ; // Set speed 10 as a variable 
     void Update()
     {
         cam.position = this.transform.position; //sets the position of cam to be equal to the position of the object(ball) to which the script is linked
         
         if(Input.GetKey(KeyCode.UpArrow)){ // checks if uparrow is pressed
-            this.transform.Translate(Vector3.forward*10*Time.deltaTime);
+            this.transform.Translate(Vector3.forward*10*Time.deltaTime);      // Change 10 to speed for the below code
         }
         if(Input.GetKey(KeyCode.DownArrow)){ // checks if downarrow is pressed
             this.transform.Translate(Vector3.back*10*Time.deltaTime);
