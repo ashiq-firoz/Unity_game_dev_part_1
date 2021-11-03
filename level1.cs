a58c8c81996bbf78ff90f29a1b27d0e58d3b6796
Ashiq Firoz <64600806+ashiq-firoz@users.noreply.github.com>
2021-11-03 21:57:41 +0530

    Part 4

--- level1.cs
+++ level1.cs
@@ -1,15 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class level1 : MonoBehaviour
 {
+    public GameObject canvas;
+    public Text text;
+
     void Start(){
-        PlayerPrefs.SetInt("final_count",0);   //PlayerPrefs database in unity
-        Debug.Log(PlayerPrefs.GetInt("final_count"));  //to print the value in the database on the console in unity editor
+        PlayerPrefs.SetInt("count",0);
+        Debug.Log(PlayerPrefs.GetInt("count"));
     }
+
     void OnCollisionEnter(Collision collision){
-        PlayerPrefs.SetInt("final_count",PlayerPrefs.GetInt("final_count")+1);  //Incrementing value of final_count when an object enters the final point.
-        Debug.Log(PlayerPrefs.GetInt("final_count"));    //to print the value in the database on the console in unity editor
-   }
+        if(collision.collider.tag=="Player"){
+            PlayerPrefs.SetInt("count",PlayerPrefs.GetInt("count")+1);
+            Debug.Log("Player arrived");
+            canvas.SetActive(true);
+            text.text= ""+PlayerPrefs.GetInt("count")+"st";
+        }
+        if(collision.collider.tag=="enemy"){
+            PlayerPrefs.SetInt("count",PlayerPrefs.GetInt("count")+1);
+        }
+        
+    }
+
+    public void next(){
+        SceneManager.LoadScene("Home");
+    }
 }
